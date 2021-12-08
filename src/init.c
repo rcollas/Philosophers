bee2f7ccb7a099a6d3504201b0ce8d4c27afca1a
rcollas <rcollas@student.42.fr>
2021-12-08 18:34:52 +0100

    norm

--- src/init.c
+++ src/init.c
@@ -7,5 +7,6 @@ void	init_table(t_var *var, char **argv)
 	var->timeToDie = atol(argv[2]);
 	var->timeToEat = atoi(argv[3]);
 	var->timeToSleep = atoi(argv[4]);
-	var->philosophers = (pthread_t *)calloc(sizeof(pthread_t), var->numberOfPhilosophers + 1);
+	var->philosophers = (pthread_t *)calloc(sizeof(pthread_t),
+			var->numberOfPhilosophers + 1);
 }
--- src/utils.c
+++ src/utils.c
@@ -1,36 +1,36 @@
 #include "../include/philosophers.h"
 
-size_t  str_len(char *str)
+size_t	str_len(char *str)
 {
-    size_t  len;
+	size_t	len;
 
-    len = 0;
-    while (str[len])
-        len++;
-    return (len);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
 
-void put_str_fd(char *str, int fd)
+void	put_str_fd(char *str, int fd)
 {
-    write (fd, str, str_len(str));
+	write (fd, str, str_len(str));
 }
 
-_Bool is_digit(char c)
+_Bool	is_digit(char c)
 {
-    if (c >= '0' && c <= '9')
-        return (TRUE);
-    return (FALSE);
+	if (c >= '0' && c <= '9')
+		return (TRUE);
+	return (FALSE);
 }
 
-_Bool is_not_numeric(char *str)
+_Bool	is_not_numeric(char *str)
 {
-    size_t  i;
+	size_t	i;
 
-    i = -1;
-    while (is_digit(str[++i]))
-    {
-        if (str[i + 1] == '\0')
-            return (FALSE);
-    }
-    return (TRUE);
+	i = -1;
+	while (is_digit(str[++i]))
+	{
+		if (str[i + 1] == '\0')
+			return (FALSE);
+	}
+	return (TRUE);
 }
