0dcb2fc269826c797a3bee085d017db776c0a70c
ModXPal <collas.robin@gmail.com>
2021-12-13 18:44:49 +0100

    before pull

--- src/main.c
+++ src/main.c
@@ -3,44 +3,50 @@
 pthread_mutex_t	mutex;
 int	i;
 t_var	var[1];
+int is_sleeping = 0;
 
-void	*incremente(void *k)
+void	*eat(void *l)
 {
 	struct timeval start, end;
+	int k;
+	k = *(int *)l;
 
-	if (var->numberOfForks >= 2) {
-		var->numberOfForks -= 2;
-		//printf("seconds = %ld, micro seconds = %ld\n", time.tv_sec, time.tv_usec);
-		printf("Philosophers %d is eating\n", *((int *)k));
-		gettimeofday(&start, NULL);
-		usleep(var->timeToEat);
-		var->numberOfForks += 2;
-		usleep(var->timeToSleep);
-		gettimeofday(&end, NULL);
-		printf("time = %ld\n", end.tv_usec - start.tv_usec);
-		printf("time to die = %ld\n", var->timeToDie);
-		if (end.tv_usec - start.tv_usec > var->timeToDie)
-			exit (1);
-	}
-	else
+	//printf("seconds = %ld, micro seconds = %ld\n", time.tv_sec, time.tv_usec);
+	gettimeofday(&start, NULL);
+	if (k % 2 == 0 && !is_sleeping) {
 		pthread_mutex_lock(&mutex);
-	pthread_mutex_unlock(&mutex);
+		usleep(5);
+		printf("Philosophers %d is eating\n", k);
+		usleep(var->timeToEat);
+	}
+	else {
+		pthread_mutex_unlock(&mutex);
+		printf("Philosophers %d is eating\n", k);
+	}
+
+	gettimeofday(&end, NULL);
 	return (NULL);
 }
 
 int main(int argc, char **argv)
 {
 	i = -1;
+	int	k;
     if (check_input(argc, argv) == ERROR)
 		return (ERROR);
 	init_table(var, argv);
 	pthread_mutex_init(&mutex, NULL);
-	while (1) {
-		for (int k = 0; k <= var->numberOfPhilosophers; k++)
-			pthread_create(&(var->philosophers)[k], NULL, &incremente, (void *)&k);
-		for (int j = 0; j <= var->numberOfPhilosophers; ++j)
-			pthread_join(var->philosophers[j], NULL);
+	//while (1) {
+	k = 0;
+	while (++k <= var->numberOfPhilosophers)
+	{
+		pthread_create(&(var->philosophers)[k], NULL, &eat, (void *) &k);
 	}
+	k = 0;
+	while (++k <= var->numberOfPhilosophers) {
+		pthread_join(var->philosophers[k], NULL);
+	}
+	//}
 	pthread_mutex_destroy(&mutex);
     return (0);
 }
